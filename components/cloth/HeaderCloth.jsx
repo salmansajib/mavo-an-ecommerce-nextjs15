ebdcc720d2976a8c1b9a5da79f139fc931e5a5a4
Salman-sajib <sfsajib@gmail.com>
2025-03-17 20:54:54 +0600

    refactor the header navigation data

--- components/cloth/HeaderCloth.jsx
+++ components/cloth/HeaderCloth.jsx
@@ -14,216 +14,11 @@ import {
   Plus,
 } from "lucide-react";
 
-const navItems = [
-  {
-    id: 1,
-    label: "Home",
-    link: "/",
-    subItems: [
-      { id: 11, label: "Home 01", link: "/" },
-      { id: 12, label: "Home 02", link: "index2.html" },
-      { id: 13, label: "Home 03", link: "index3.html" },
-      { id: 14, label: "Home 04", link: "index4.html" },
-      { id: 15, label: "Home 05", link: "index5.html" },
-      { id: 16, label: "Home 06", link: "index6.html" },
-    ],
-  },
-  {
-    id: 2,
-    label: "Shop",
-    link: "#",
-    subItems: [
-      { id: 21, label: "Bag Shop", link: "bag-shop.html" },
-      { id: 22, label: "Beauty Shop", link: "beauty-shop.html" },
-      { id: 23, label: "Cloth Shop", link: "/cloth/product" },
-      { id: 24, label: "Watch Shop", link: "watch-shop.html" },
-      { id: 25, label: "Shoes Shop", link: "shoes-shop.html" },
-      { id: 26, label: "Jewellery Shop", link: "jewellery-shop.html" },
-    ],
-  },
-  {
-    id: 3,
-    label: "Blog",
-    link: "#",
-    subItems: [
-      { id: 31, label: "Shoes Blog", link: "shoes-blog.html" },
-      { id: 32, label: "Shoes Blog2", link: "shoes-blog2.html" },
-      { id: 33, label: "Jewellery Blog", link: "jewellery-blog.html" },
-      { id: 34, label: "Beauty Blog", link: "beauty-blog.html" },
-      { id: 35, label: "Bag Blog", link: "bag-blog.html" },
-      { id: 36, label: "Blog Single", link: "blog-single.html" },
-      { id: 37, label: "Blog Details", link: "blog-details.html" },
-    ],
-  },
-  {
-    id: 4,
-    label: "Pages",
-    link: "#",
-    subItems: [
-      { id: 41, label: "About Us Beauty", link: "about-us-beauty.html" },
-      { id: 42, label: "About Us Bag", link: "about-us-bag.html" },
-      { id: 43, label: "About Us Clothing", link: "about-us-clothing.html" },
-      { id: 44, label: "Cart", link: "cart.html" },
-      { id: 45, label: "Checkout", link: "checkout.html" },
-      { id: 46, label: "Faq", link: "faq.html" },
-      { id: 47, label: "Thank You", link: "thank-you.html" },
-      {
-        id: 48,
-        label: "Watch Product Single",
-        link: "watch-product-single.html",
-      },
-      {
-        id: 49,
-        label: "Jewellery Product Single",
-        link: "jewellery-product-single.html",
-      },
-      {
-        id: 50,
-        label: "Cloth Product Single",
-        link: "cloth-product-single.html",
-      },
-      {
-        id: 51,
-        label: "Beauty Product Single",
-        link: "beauty-product-single.html",
-      },
-      { id: 52, label: "Bag Product Single", link: "bag-product-single.html" },
-      { id: 53, label: "404 Page", link: "404-page.html" },
-    ],
-  },
-  {
-    id: 5,
-    label: "Contact",
-    link: "#",
-    subItems: [
-      { id: 51, label: "Contact", link: "contact.html" },
-      { id: 52, label: "Contact Us", link: "contact-us.html" },
-    ],
-  },
-];
-const navItemsMobile = [
-  {
-    id: 1,
-    label: "Home",
-    link: "/",
-    subItems: [
-      { id: 11, label: "Home 01", link: "/" },
-      { id: 12, label: "Home 02", link: "index2.html" },
-      { id: 13, label: "Home 03", link: "index3.html" },
-      { id: 14, label: "Home 04", link: "index4.html" },
-      { id: 15, label: "Home 05", link: "index5.html" },
-      { id: 16, label: "Home 06", link: "index6.html" },
-    ],
-  },
-  {
-    id: 2,
-    label: "Shop",
-    link: "#",
-    subItems: [
-      { id: 21, label: "Bag Shop", link: "bag-shop.html" },
-      { id: 22, label: "Beauty Shop", link: "beauty-shop.html" },
-      { id: 23, label: "Cloth Shop", link: "/cloth/product" },
-      { id: 24, label: "Watch Shop", link: "watch-shop.html" },
-      { id: 25, label: "Shoes Shop", link: "shoes-shop.html" },
-      { id: 26, label: "Jewellery Shop", link: "jewellery-shop.html" },
-    ],
-  },
-  {
-    id: 3,
-    label: "Blog",
-    link: "#",
-    subItems: [
-      { id: 31, label: "Shoes Blog", link: "shoes-blog.html" },
-      { id: 32, label: "Shoes Blog2", link: "shoes-blog2.html" },
-      { id: 33, label: "Jewellery Blog", link: "jewellery-blog.html" },
-      { id: 34, label: "Beauty Blog", link: "beauty-blog.html" },
-      { id: 35, label: "Bag Blog", link: "bag-blog.html" },
-      { id: 36, label: "Blog Single", link: "blog-single.html" },
-      { id: 37, label: "Blog Details", link: "blog-details.html" },
-    ],
-  },
-  {
-    id: 4,
-    label: "Pages",
-    link: "#",
-    subItems: [
-      { id: 41, label: "About Us Beauty", link: "about-us-beauty.html" },
-      { id: 42, label: "About Us Bag", link: "about-us-bag.html" },
-      { id: 43, label: "About Us Clothing", link: "about-us-clothing.html" },
-      { id: 44, label: "Cart", link: "cart.html" },
-      { id: 45, label: "Checkout", link: "checkout.html" },
-      { id: 46, label: "Faq", link: "faq.html" },
-      { id: 47, label: "Thank You", link: "thank-you.html" },
-      {
-        id: 48,
-        label: "Watch Product Single",
-        link: "watch-product-single.html",
-      },
-      {
-        id: 49,
-        label: "Jewellery Product Single",
-        link: "jewellery-product-single.html",
-      },
-      {
-        id: 50,
-        label: "Cloth Product Single",
-        link: "cloth-product-single.html",
-      },
-      {
-        id: 51,
-        label: "Beauty Product Single",
-        link: "beauty-product-single.html",
-      },
-      { id: 52, label: "Bag Product Single", link: "bag-product-single.html" },
-      { id: 53, label: "404 Page", link: "404-page.html" },
-    ],
-  },
-  {
-    id: 5,
-    label: "Contact",
-    link: "#",
-    subItems: [
-      { id: 51, label: "Contact", link: "contact.html" },
-      { id: 52, label: "Contact Us", link: "contact-us.html" },
-    ],
-  },
-  {
-    id: 6,
-    label: "Wish List",
-    link: "#",
-  },
-  {
-    id: 7,
-    label: "My Account",
-    link: "#",
-  },
-];
-const langAndCurrencyItems = [
-  {
-    id: 1,
-    label: "eng",
-    link: "/",
-    subItems: [
-      { id: 11, label: "es", link: "/" },
-      { id: 12, label: "ru", link: "/" },
-      { id: 13, label: "ku", link: "/" },
-      { id: 14, label: "de", link: "/" },
-      { id: 15, label: "it", link: "/" },
-      { id: 16, label: "ar", link: "/" },
-    ],
-  },
-  {
-    id: 2,
-    label: "usd",
-    link: "/",
-    subItems: [
-      { id: 21, label: "dzd", link: "/" },
-      { id: 22, label: "eur", link: "/" },
-      { id: 23, label: "aoa", link: "/" },
-      { id: 24, label: "cny", link: "/" },
-    ],
-  },
-];
+import {
+  navItems,
+  navItemsMobile,
+  langAndCurrencyItems,
+} from "@/data/navigationData";
 
 const HeaderCloth = () => {
   const [hoveredMenuId, setHoveredMenuId] = useState(null);
@@ -453,11 +248,14 @@ const HeaderCloth = () => {
             onClick={() => setShowMobileMenu(false)} // Closes menu when clicked
           />
         )}
+
         {/* Mobile Navigation */}
         <nav
           className={`${
-            showMobileMenu ? "translate-x-0" : "translate-x-[100%]"
-          } fixed z-50 w-full max-w-[450px] h-full top-0 right-0 bg-[#000] font-marcellus lg:hidden p-[30px] transition-transform ease-in-out duration-500`}
+            showMobileMenu
+              ? "translate-x-0 opacity-100"
+              : "translate-x-[100%] opacity-0"
+          } fixed z-50 w-full max-w-[450px] h-full top-0 right-0 bg-[#000] font-marcellus lg:hidden p-[30px] transition-all ease-in-out duration-500`}
         >
           <div className="flex items-center justify-between border-b border-[#fff]/40 pb-[20px]">
             <Logo color="light" width={90} />
@@ -474,21 +272,48 @@ const HeaderCloth = () => {
             <ul>
               {navItemsMobile.map((item) => (
                 <li key={item.id}>
-                  <button
-                    className="!text-[#fff] w-full py-3 text-base flex items-center justify-between"
-                    onClick={() => handleToggleSubmenu(item.id)}
-                  >
-                    {item.label}
-                    {item.subItems && (
-                      <Plus
-                        size={20}
-                        color="#fff"
-                        className={`${
-                          showMobileSubmenu === item.id ? "-rotate-45" : ""
-                        } transition-all duration-500 ease-in-out`}
-                      />
-                    )}
-                  </button>
+                  {item.subItems ? (
+                    // If sub-items exist, toggle submenu on click
+                    <button
+                      className="!text-[#fff] w-full py-3 text-base flex items-center justify-between"
+                      onClick={() => handleToggleSubmenu(item.id)}
+                    >
+                      {item.label}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="lucide lucide-plus transition-all duration-300 ease-in-out"
+                      >
+                        {/* Horizontal line: Always visible */}
+                        <path d="M5 12h14" />
+                        {/* Vertical line: Disappears when submenu is open  */}
+                        <path
+                          d="M12 5v14"
+                          className={`transition-all duration-300 ${
+                            showMobileSubmenu === item.id
+                              ? "stroke-[0]"
+                              : "stroke-[2]"
+                          }`}
+                        />
+                      </svg>
+                    </button>
+                  ) : (
+                    // If no sub-items, use Link for navigation
+                    <Link
+                      href={item.link}
+                      className="!text-[#fff] w-full py-3 text-base block"
+                      onClick={() => setShowMobileMenu(false)} // Close menu when clicking a direct link
+                    >
+                      {item.label}
+                    </Link>
+                  )}
 
                   {item.subItems && (
                     <ul
---
+++ data/navigationData.js
@@ -0,0 +1,212 @@
+export const navItems = [
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+    subItems: [
+      { id: 11, label: "Home 01", link: "/" },
+      { id: 12, label: "Home 02", link: "index2.html" },
+      { id: 13, label: "Home 03", link: "index3.html" },
+      { id: 14, label: "Home 04", link: "index4.html" },
+      { id: 15, label: "Home 05", link: "index5.html" },
+      { id: 16, label: "Home 06", link: "index6.html" },
+    ],
+  },
+  {
+    id: 2,
+    label: "Shop",
+    link: "#",
+    subItems: [
+      { id: 21, label: "Bag Shop", link: "bag-shop.html" },
+      { id: 22, label: "Beauty Shop", link: "beauty-shop.html" },
+      { id: 23, label: "Cloth Shop", link: "/cloth/product" },
+      { id: 24, label: "Watch Shop", link: "watch-shop.html" },
+      { id: 25, label: "Shoes Shop", link: "shoes-shop.html" },
+      { id: 26, label: "Jewellery Shop", link: "jewellery-shop.html" },
+    ],
+  },
+  {
+    id: 3,
+    label: "Blog",
+    link: "#",
+    subItems: [
+      { id: 31, label: "Shoes Blog", link: "shoes-blog.html" },
+      { id: 32, label: "Shoes Blog2", link: "shoes-blog2.html" },
+      { id: 33, label: "Jewellery Blog", link: "jewellery-blog.html" },
+      { id: 34, label: "Beauty Blog", link: "beauty-blog.html" },
+      { id: 35, label: "Bag Blog", link: "bag-blog.html" },
+      { id: 36, label: "Blog Single", link: "blog-single.html" },
+      { id: 37, label: "Blog Details", link: "blog-details.html" },
+    ],
+  },
+  {
+    id: 4,
+    label: "Pages",
+    link: "#",
+    subItems: [
+      { id: 41, label: "About Us Beauty", link: "about-us-beauty.html" },
+      { id: 42, label: "About Us Bag", link: "about-us-bag.html" },
+      { id: 43, label: "About Us Clothing", link: "about-us-clothing.html" },
+      { id: 44, label: "Cart", link: "cart.html" },
+      { id: 45, label: "Checkout", link: "checkout.html" },
+      { id: 46, label: "Faq", link: "faq.html" },
+      { id: 47, label: "Thank You", link: "thank-you.html" },
+      {
+        id: 48,
+        label: "Watch Product Single",
+        link: "watch-product-single.html",
+      },
+      {
+        id: 49,
+        label: "Jewellery Product Single",
+        link: "jewellery-product-single.html",
+      },
+      {
+        id: 50,
+        label: "Cloth Product Single",
+        link: "cloth-product-single.html",
+      },
+      {
+        id: 51,
+        label: "Beauty Product Single",
+        link: "beauty-product-single.html",
+      },
+      { id: 52, label: "Bag Product Single", link: "bag-product-single.html" },
+      { id: 53, label: "404 Page", link: "404-page.html" },
+    ],
+  },
+  {
+    id: 5,
+    label: "Contact",
+    link: "#",
+    subItems: [
+      { id: 51, label: "Contact", link: "contact.html" },
+      { id: 52, label: "Contact Us", link: "contact-us.html" },
+    ],
+  },
+];
+
+export const navItemsMobile = [
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+    subItems: [
+      { id: 11, label: "Home 01", link: "/" },
+      { id: 12, label: "Home 02", link: "index2.html" },
+      { id: 13, label: "Home 03", link: "index3.html" },
+      { id: 14, label: "Home 04", link: "index4.html" },
+      { id: 15, label: "Home 05", link: "index5.html" },
+      { id: 16, label: "Home 06", link: "index6.html" },
+    ],
+  },
+  {
+    id: 2,
+    label: "Shop",
+    link: "#",
+    subItems: [
+      { id: 21, label: "Bag Shop", link: "bag-shop.html" },
+      { id: 22, label: "Beauty Shop", link: "beauty-shop.html" },
+      { id: 23, label: "Cloth Shop", link: "/cloth/product" },
+      { id: 24, label: "Watch Shop", link: "watch-shop.html" },
+      { id: 25, label: "Shoes Shop", link: "shoes-shop.html" },
+      { id: 26, label: "Jewellery Shop", link: "jewellery-shop.html" },
+    ],
+  },
+  {
+    id: 3,
+    label: "Blog",
+    link: "#",
+    subItems: [
+      { id: 31, label: "Shoes Blog", link: "shoes-blog.html" },
+      { id: 32, label: "Shoes Blog2", link: "shoes-blog2.html" },
+      { id: 33, label: "Jewellery Blog", link: "jewellery-blog.html" },
+      { id: 34, label: "Beauty Blog", link: "beauty-blog.html" },
+      { id: 35, label: "Bag Blog", link: "bag-blog.html" },
+      { id: 36, label: "Blog Single", link: "blog-single.html" },
+      { id: 37, label: "Blog Details", link: "blog-details.html" },
+    ],
+  },
+  {
+    id: 4,
+    label: "Pages",
+    link: "#",
+    subItems: [
+      { id: 41, label: "About Us Beauty", link: "about-us-beauty.html" },
+      { id: 42, label: "About Us Bag", link: "about-us-bag.html" },
+      { id: 43, label: "About Us Clothing", link: "about-us-clothing.html" },
+      { id: 44, label: "Cart", link: "cart.html" },
+      { id: 45, label: "Checkout", link: "checkout.html" },
+      { id: 46, label: "Faq", link: "faq.html" },
+      { id: 47, label: "Thank You", link: "thank-you.html" },
+      {
+        id: 48,
+        label: "Watch Product Single",
+        link: "watch-product-single.html",
+      },
+      {
+        id: 49,
+        label: "Jewellery Product Single",
+        link: "jewellery-product-single.html",
+      },
+      {
+        id: 50,
+        label: "Cloth Product Single",
+        link: "cloth-product-single.html",
+      },
+      {
+        id: 51,
+        label: "Beauty Product Single",
+        link: "beauty-product-single.html",
+      },
+      { id: 52, label: "Bag Product Single", link: "bag-product-single.html" },
+      { id: 53, label: "404 Page", link: "404-page.html" },
+    ],
+  },
+  {
+    id: 5,
+    label: "Contact",
+    link: "#",
+    subItems: [
+      { id: 51, label: "Contact", link: "contact.html" },
+      { id: 52, label: "Contact Us", link: "contact-us.html" },
+    ],
+  },
+  {
+    id: 6,
+    label: "Wish List",
+    link: "/wish-list",
+  },
+  {
+    id: 7,
+    label: "My Account",
+    link: "/user-account",
+  },
+];
+
+export const langAndCurrencyItems = [
+  {
+    id: 1,
+    label: "eng",
+    link: "/",
+    subItems: [
+      { id: 11, label: "es", link: "/" },
+      { id: 12, label: "ru", link: "/" },
+      { id: 13, label: "ku", link: "/" },
+      { id: 14, label: "de", link: "/" },
+      { id: 15, label: "it", link: "/" },
+      { id: 16, label: "ar", link: "/" },
+    ],
+  },
+  {
+    id: 2,
+    label: "usd",
+    link: "/",
+    subItems: [
+      { id: 21, label: "dzd", link: "/" },
+      { id: 22, label: "eur", link: "/" },
+      { id: 23, label: "aoa", link: "/" },
+      { id: 24, label: "cny", link: "/" },
+    ],
+  },
+];
