842924b09df20df40378c1c9c79c46657884be00
Salman-sajib <sfsajib@gmail.com>
2025-05-13 17:39:11 +0600

    single item card for cloth

--- components/cloth/ProductSectionSecondCloth.jsx
+++ components/cloth/ProductSectionSecondCloth.jsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { useProducts } from "@/hooks/useProducts";
 import LoaderSpinner from "../LoaderSpinner";
+import ItemCard from "./ItemCard";
 
 const FEATURED_PRODUCTS_LIMIT = 4;
 
@@ -49,95 +49,7 @@ const ProductSectionSecondCloth = () => {
             <div className="row">
               {products.map((product) => (
                 <div className="col-lg-6 col-md-6 col-sm-6" key={product.id}>
-                  <div className="mavo-product-single mavo-md-mb-55">
-                    <div className="mavo-product-info">
-                      <div className="mavo-product-info-img">
-                        <Image
-                          src={product.variants[0].images[0]}
-                          alt={product.name}
-                          width={500}
-                          height={500}
-                          quality={100}
-                          className="w-[500px] h-auto"
-                        />
-                      </div>
-                      {product.tags && (
-                        <div
-                          className={`mavo-product-tag ${product.tags.toLowerCase()}`}
-                        >
-                          <span>{product.tags}</span>
-                        </div>
-                      )}
-                      <div className="mavo-product-social">
-                        <ul>
-                          <li>
-                            <Link href={`/cloth/${product.id}`}>
-                              <i className="flaticon-eye"></i>
-                              <span> Quick view</span>
-                            </Link>
-                          </li>
-                          <li>
-                            <Link href={`/cloth/${product.id}`}>
-                              <i className="flaticon-star"></i>
-                              <span> Compare</span>
-                            </Link>
-                          </li>
-                          <li>
-                            <Link href={`/cloth/${product.id}`}>
-                              <i className="flaticon-sort"></i>
-                              <span> Wishlist</span>
-                            </Link>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="mavo-product-cart">
-                        <Link href={`/cloth/${product.id}`}>Buy Now</Link>
-                      </div>
-                    </div>
-                    <div className="mavo-product-meta mavo-mt-35 mavo-mb-40">
-                      <div className="mavo-product-category">
-                        <h6>
-                          <Link
-                            className="mavo-product-title-link hover:!text-[#c9a96b]"
-                            href={`/cloth/${product.id}`}
-                          >
-                            {product.name}
-                          </Link>
-                        </h6>
-                        <span className="product-sub-title font-prata">
-                          ${product.base_price.toFixed(2)}
-                        </span>
-                      </div>
-                      <div className="mavo-product-rating">
-                        <div className="rating-star">
-                          {[...Array(5)].map((_, i) => (
-                            <i key={i} className="flaticon-star-1"></i>
-                          ))}
-                          <Link href="#" className="rating-count font-prata">
-                            {product.reviews} Reviews
-                          </Link>
-                        </div>
-                        <div className="mavo-product-color-btn">
-                          <ul>
-                            {product.variants.map((variant, index) => (
-                              <li key={index}>
-                                <Link
-                                  href={`/cloth/${product.id}`}
-                                  style={{
-                                    display: "block",
-                                    width: "20px",
-                                    height: "20px",
-                                    backgroundColor: variant.color_code,
-                                    borderRadius: "50%",
-                                  }}
-                                ></Link>
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <ItemCard product={product} />
                 </div>
               ))}
             </div>
--- components/cloth/product-details/SimilarProducts.jsx
+++ components/cloth/product-details/SimilarProducts.jsx
@@ -1,9 +1,8 @@
 "use client";
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
+import ItemCard from "../ItemCard";
 
 const SimilarProducts = ({ similarProducts }) => {
   return (
@@ -31,98 +30,7 @@ const SimilarProducts = ({ similarProducts }) => {
         >
           {similarProducts.map((product) => (
             <SwiperSlide key={product.id}>
-              <div className="mavo-product-single mavo-md-mb-55">
-                <div className="mavo-product-info">
-                  <div className="mavo-product-info-img">
-                    <Image
-                      src={product.variants[0].images[0]}
-                      alt={product.name}
-                      width={500}
-                      height={500}
-                      quality={100}
-                      style={{
-                        width: "500px",
-                        height: "auto",
-                      }}
-                    />
-                  </div>
-                  {product.tags && (
-                    <div
-                      className={`mavo-product-tag ${product.tags.toLowerCase()}`}
-                    >
-                      <span>{product.tags}</span>
-                    </div>
-                  )}
-                  <div className="mavo-product-social">
-                    <ul>
-                      <li>
-                        <Link href={`/cloth/${product.id}`}>
-                          <i className="flaticon-eye"></i>
-                          <span> Quick view</span>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href={`/cloth/${product.id}`}>
-                          <i className="flaticon-star"></i>
-                          <span> Compare</span>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href={`/cloth/${product.id}`}>
-                          <i className="flaticon-sort"></i>
-                          <span> Wishlist</span>
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="mavo-product-cart">
-                    <Link href={`/cloth/${product.id}`}>Buy Now</Link>
-                  </div>
-                </div>
-                <div className="mavo-product-meta mavo-mt-35 mavo-mb-40">
-                  <div className="mavo-product-category">
-                    <h6>
-                      <Link
-                        className="mavo-product-title-link"
-                        href={`/cloth/${product.id}`}
-                      >
-                        {product.name}
-                      </Link>
-                    </h6>
-                    <span className="product-sub-title font-prata">
-                      ${product.base_price.toFixed(2)}
-                    </span>
-                  </div>
-                  <div className="mavo-product-rating">
-                    <div className="rating-star">
-                      {[...Array(5)].map((_, i) => (
-                        <i key={i} className="flaticon-star-1"></i>
-                      ))}
-                      <Link href="#" className="rating-count font-prata">
-                        {product.reviews} Reviews
-                      </Link>
-                    </div>
-                    <div className="mavo-product-color-btn">
-                      <ul>
-                        {product.variants.map((variant, index) => (
-                          <li key={index}>
-                            <Link
-                              href="#"
-                              style={{
-                                display: "block",
-                                width: "20px",
-                                height: "20px",
-                                backgroundColor: variant.color_code,
-                                borderRadius: "50%",
-                              }}
-                            ></Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <ItemCard product={product} />
             </SwiperSlide>
           ))}
         </Swiper>
