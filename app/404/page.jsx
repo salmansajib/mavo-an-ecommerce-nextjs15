b055e4e0be77fbc5b8832818987e753e3d033e43
Salman-sajib <sfsajib@gmail.com>
2025-11-05 10:39:21 +0600

    feature: 404 page for the nav to show a 404 page by clicking on the nav

---
+++ app/404/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import BreadcrumbContact from "@/components/contact/BreadcrumbContact";
+import NotFoundComponent from "@/components/not-found/NotFoundComponent";
+import Header from "@/components/shoe/header/Header";
+import Footer from "@/components/shoe/Footer";
+
+export default function NotFoundPage() {
+  return (
+    <div className="pt-[120px]">
+      <Header />
+      <BreadcrumbContact
+        items={[
+          { label: "Home", href: "/" },
+          { label: "Page", href: "/some-page" },
+          { label: "404" },
+        ]}
+      />
+      <NotFoundComponent />
+      <Footer />
+    </div>
+  );
+}
